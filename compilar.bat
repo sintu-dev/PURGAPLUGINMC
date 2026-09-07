95e3562082b75063d2420df14369f56137d69a4c
sintu-dev <sintujoanne@gmail.com>
2026-09-06 20:19:17 -0600

    Delete compilar.bat

--- compilar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo =======================================
-echo Compilando PurgaPlugin...
-echo =======================================
-powershell -ExecutionPolicy Bypass -File "%~dp0compile.ps1"
-echo.
-pause
